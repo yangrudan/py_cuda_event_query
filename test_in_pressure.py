eeaabcd189d39a3dd298d7b37a77b5cf15863297
Yang <1549029765@qq.com>
2025-09-26 10:26:15 +0800

    feat: modify get stream position

--- test_in_pressure.py
+++ test_in_pressure.py
@@ -19,9 +19,9 @@ def main(index):
 
     #dist.barrier(device_ids=[device.index])  # 指定设备，消除警告
     # 获取当前CUDA流（NCCL通信默认使用当前流）
-    current_stream = torch.cuda.current_stream(torch.cuda.current_device())
-    # 将流指针转换为整数传递给C++
-    stream_ptr = current_stream.cuda_stream
+#    current_stream = torch.cuda.current_stream(torch.cuda.current_device())
+#    # 将流指针转换为整数传递给C++
+#    stream_ptr = current_stream.cuda_stream
 
     timer = AsyncTimer()  # 实例化计时器
     gradient = torch.tensor([rank, rank, rank], dtype=torch.float32, device=device)
@@ -39,6 +39,10 @@ def main(index):
     else:
         # 异步模式：启动all_reduce后立即返回work对象，不阻塞
         work = dist.all_reduce(gradient, op=dist.ReduceOp.SUM, async_op=True)
+        # 获取当前CUDA流（NCCL通信默认使用当前流）
+        current_stream = torch.cuda.current_stream(torch.cuda.current_device())
+        # 将流指针转换为整数传递给C++
+        stream_ptr = current_stream.cuda_stream
         # 异步等待通信完成（仅为了确保梯度计算完成，不影响计时）
         timer.start(stream_ptr)  # 1. 记录开始事件（GPU时间戳）
         work.wait()
@@ -51,7 +55,7 @@ def main(index):
         # 等待期间可执行其他CPU/GPU任务（体现异步计时优势）
         time.sleep(0.1)  # 降低CPU占用
 
-    # 5. 获取并打印耗时
+    # 5. 校验时间
     elapsed = timer.get_elapsed()
     if rank == 1:
         if elapsed > 100:
@@ -64,6 +68,11 @@ def main(index):
         print(f"=======current index is {index}=======")
     # print(f" \n!!!RANK {rank} - 总耗时: {elapsed:.4f}ms")
     
+    if i == index - 1:
+        # 6. 清理资源
+        del timer  # 销毁计时器（调用C++析构函数）
+        dist.destroy_process_group()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -76,9 +85,6 @@ if __name__ == "__main__":
     world_size = dist.get_world_size()
     device = torch.device(f'cuda:{rank % torch.cuda.device_count()}')
     torch.cuda.set_device(device)  # 显式设置当前进程的GPU设备，消除警告
-    for i in range(5000):
+    for i in range(50):
          main(i)
 
-    # 6. 清理资源
-    del timer  # 销毁计时器（调用C++析构函数）
-    dist.destroy_process_group()
